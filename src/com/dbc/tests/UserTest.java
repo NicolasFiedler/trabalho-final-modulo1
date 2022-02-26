17396e32b0838be53901803ef945ff49d25f8b87
NicolasFiedler <nicolasxfiedler@gmail.com>
2022-02-26 17:43:24 -0300

    fix: cpf/heranca

--- src/com/dbc/tests/UserTest.java
+++ src/com/dbc/tests/UserTest.java
@@ -1,11 +1,19 @@
 package com.dbc.tests;
 
 import com.dbc.entities.user.Person;
+import com.dbc.entities.user.User;
 import org.junit.Test;
 
 public class UserTest {
     @Test
     public void testarUsuario (){
         Person person = new Person("nome", "email", "cpf");
+        User.userListPrint();
+
+        User user = User.getUserById(0);
+        if (user instanceof Person){
+            Person person1 = (Person) user;
+            System.out.println(person1.getCpf());
+        }
     }
 }
