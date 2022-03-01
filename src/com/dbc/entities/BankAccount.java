bce384b320b33272fd21a84589d9d0078be8e8b7
Ana Vitória Göcthel <anagocthel@gmail.com>
2022-03-01 12:09:18 -0300

    feat: criação dos testes das classes bank account e donate

--- src/com/dbc/entities/BankAccount.java
+++ src/com/dbc/entities/BankAccount.java
@@ -6,9 +6,26 @@ public class BankAccount {
 
     public BankAccount(){
     }
+
     public BankAccount(String number, String agency){
         this.number=number;
         this.agency=agency;
+    }
+
+    public static Boolean simulatePayment (String str) {
+        if (str.equalsIgnoreCase("válido")){
+            return true;
+        }
+        return false;
+    }
+
+    public String simulateInvoice (Donate donate){
+        String invoice;
+        invoice = "Nota fiscal:\n"+ "Nome: \t"+ donate.getNameDonator()+"\n"+" Valor: \t"+
+               donate.getDonateValue()+"\n"+"Cpf: \t"+ donate.getCpfDonator()+ "\n Obrigado pela colaboração!";
+
+        return invoice;
+
     }
 
 
@@ -26,21 +43,5 @@ public class BankAccount {
 
     public void setAgency(String agency) {
         this.agency = agency;
-    }
-
-    public static Boolean simulatePayment (String str) {
-        if (str.equalsIgnoreCase("válido")){
-            return true;
-        }
-        return false;
-    }
-
-    public String simulateInvoice (Donate donate){
-        String invoice;
-        invoice = "Nota fiscal:\n"+ "Nome: \t"+ donate.getNameDonator()+"\n"+" Valor: \t"+
-               donate.getDonateValue()+"\n"+"Cpf: \t"+ donate.getCpfDonator()+ "\n Obrigado pela colaboração!";
-
-        return invoice;
-
     }
 }
--- src/com/dbc/tests/DonateTest.java
+++ src/com/dbc/tests/DonateTest.java
@@ -1,5 +1,48 @@
 package com.dbc.tests;
 
-public class DonateTest {
+import com.dbc.entities.BankAccount;
+import com.dbc.entities.Donate;
+import com.dbc.entities.Request;
+import org.junit.Assert;
+import org.junit.Test;
 
+import java.util.Scanner;
+
+import static org.junit.Assert.*;
+
+public class DonateTest {
+    @Test
+    public void deveTestarCreateDonate() {
+        Donate donate=new Donate();
+        Integer id= 2;
+        boolean var = false;
+
+        String payStatus = "válido";
+
+        if (BankAccount.simulatePayment(payStatus)) {
+
+            donate.setNameDonator("ana");
+            donate.setCpfDonator("6533");
+            donate.setEmailDonator("ana@gmail");
+            donate.setDonateValue(30.0);
+            donate.setAccoutInformation("akjhksnkj");
+            String description = "SIM";
+
+            var = true;
+        }
+        assertTrue(var);
+    }
+
+    @Test
+    public void deveTestarDescription(){
+        Donate donate=new Donate();
+        String description= "sim";
+
+        if (description.equalsIgnoreCase("sim")) {
+            donate.setDescription("BOA SORTE");
+        } else {
+            donate.setDescription(null);
+        }
+        assertEquals(donate.getDescription(), "BOA SORTE");
+    }
 }
